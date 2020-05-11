74fc9bd3fa7c16b32c9aafcd5d0eef31324a9964
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2020-05-11 03:00:00 +0200

    Add files via upload

--- springerebooks.R
+++ springerebooks.R
@@ -16,7 +16,8 @@ cat("", file="download.log")
 
 for (i in 1:N) {
     nombre=libros$BookTitle[i]
-    for (char in c('.','/',',',':',';',' ','-','@','®')) {  # Limpiamos nombre
+    # Permitimos & y ++
+    for (char in c('.','/',',',':',';',' ','-','_','@','®')) {  # Limpiamos nombre
         nombre=gsub(char, "", nombre, fixed=T)
     }
     numero=paste0(iif(i<10, "00", iif(i<100, "0", "")), i)
